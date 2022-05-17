15279b0836a527b9dc6d70a1fee7ebd91e9e9ee2
anthonykpn <anthonykpnguyen@gmail.com>
2022-05-17 15:39:35 -0700

    added main

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "Artist.h"
+
+using namespace std; 
+
+int main() {
+	string artist; 
+	Artist a; 	
+
+
+	cout << "Who is your favorite artist or band?" << endl; 
+	getline(cin, artist); 
+	a.getFavArtist; 
+
+
+
+}
