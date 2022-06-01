5820e7aa6094ebe8802bfc72ea27a77365da98bf
girumyaye <gyaye001@ucr.edu>
2022-06-01 08:14:17 -0700

    updated Genre.hpp

--- Genre.hpp
+++ Genre.hpp
@@ -8,15 +8,22 @@ using namespace std;
 //genre
 class Genre {
     private:
-	string genresongs;
-	string subgenresongs;
+	string genre;
+	string artist;
+        string song;
     public:
-        Genre();
-	Genre(string, string);
-	static string getFavGenreSongs();
-	static string getFavSubGenreSongs();
-	void favGenre();
-	void favSubGenre();
+	Genre(string genre, string artist, string song) {
+	    this -> genre = genre;
+            this -> artist = artist;
+            this -> song = song;
+	}
+	void favGenre(vector <map<pair<string, string>> Genre) override{
+	     map<pair<string, string<>, string>::iterator geniterator;
+             cout << "Here's a list of songs from your favorite genre: " << endl;
+	     for (geniterator = Genre.at(genre).begin(); geniterator != Genre.at(genre).end(); ++geniterator) {
+		 cout << "   (" << 1 << ") '" << geniterator -> second << "' by " << geniterator -> first.second << endl;
+             }
+         }
 };
 
 #endif
