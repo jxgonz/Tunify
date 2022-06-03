212e6507d10a487c2dd8b1f5c1cab35f539116b8
girumyaye <gyaye001@ucr.edu>
2022-06-03 12:13:09 -0700

    Genre test cases

--- Genre_test.hpp
+++ Genre_test.hpp
@@ -4,17 +4,32 @@
 #include <iostream>
 #include "gtest/gtest.h"
 #include "Genre.hpp"
+#include "Playlist.hpp"
 
 using namespace std;
 
 TEST(GenreTest, GenreTestSongs) {
-    Playlist test = new Genre(genre, artist, song);
-    EXPECT_EQ(test.getFavGenre("Bad and Boujee");
-    EXPECT_EQ(test.getFavGenre("Stir Fry");
-    EXPECT_EQ(test.getFavGenre("a lot");
-    EXPECT_EQ(test.getFavGenre("On the Run");
-    EXPECT_EQ(test.getFavGenre("Back In Blood");
+    Playlist test = new Genre("Rap", "Kendrick Lamar", "N5");
+    EXPECT_EQ(test->GetGenre(), "Rap/Hip-Hop");
 }
 
+TEST(GenreTest, GenreTestSongs) {
+    Playlist test = new Genre("RB", "Ravyn Lenae", "Venom");
+    EXPECT_EQ(test->GetGenre(), "R&B");
+}
+
+TEST(GenreTest, GenreTestSongs) {
+    Playlist test = new Genre("Pop", "Harry Styles", "Daydreaming");
+    EXPECT_EQ(test->GetGenre(), "Pop");
+}
+
+TEST(GenreTest, GenreTestSongs) {
+    Playlist test = new Genre("Country", "I'll Be Here in the Morning", "Townes Van Zandt");
+    EXPECT_EQ(test->GetGenre(), "Country");
+
+TEST(GenreTest, GenreTestSongs) {
+    Playlist test = new Genre("Latin", "Miguel", "Te Lo Dije");
+    EXPECT_EQ(test->GetGenre(), "Latin");
+}
 #endif //__GENRE_TEST_HPP__
     
