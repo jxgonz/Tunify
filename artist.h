da3fac2c888ae8788a0552350a27b6dc04f67d8d
anthonykpn <79613962+anthonykpn@users.noreply.github.com>
2022-06-02 17:51:02 -0700

    Update artist.h

--- artist.h
+++ artist.h
@@ -1,18 +1,33 @@
 #ifndef __ARTIST_H__
 #define __ARTIST_H__
-#include <iostream>
+#include "Recommender.hpp"
+#include <map>
+#include <iterator>
 
-using namespace std; 
-class Artist {
-private: 
-    string artist; 
-    string feature; 
-public: 
-    Artist(); 
-    Artist(string, string);
-    static string getFavArtist(); 
-    static string getFavFeature(); 
-    void favArtist(); 
-    void favFeature();  
-};
+class Artist : public Recommender {
+    private: 
+    string artist;
+    string song;
+    int genre;
+    string album;
+    
+    public: 
+    Artist(string artist, string song, int genre, string album) {
+        this->artist = artist;
+        this->song = song;
+        this->genre = genre;
+        this->album = album;
+    }
+void Favorite(vector<map<pair<string, string>, string>> Genre) override {
+        map<pair<string, string>, string>::iterator iter;
+        int i = 1;
+        cout << "Here's a List of Artist you might enjoy!:" << endl;
+        for (iter = Genre.at(genre).begin(); iter != Genre.at(genre).end(); ++iter) {
+            cout << "   (" << i << ") '" << iter ->first.first << "' by " << iter -> first.second << endl;
+            cout << "           '" << iter->second << " " << iter->first.first << endl << endl;
+            ++i;
+        }
+    }  
+}; 
+
 #endif 
